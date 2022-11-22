3b9f0c3a2edebe0aa5db32d22511b2eaa050b585
han <han@mail.com>
2022-11-22 18:04:18 +0800

    test

---
+++ Assets/Scripts/Simulator.cs
@@ -0,0 +1,60 @@
+using System.Collections;
+using System;
+using UnityEngine;
+using UnityEngine.Profiling;
+using System.Diagnostics;
+
+public class Simulator : IDisposable
+{
+    private int RoutineTimeFrame = 10;
+    private int RoutineCheckCount = 50;
+
+    public void Dispose()
+    {
+
+    }
+
+    // Customize the counter to prevent the stopwatch from getting timestamps too often
+    bool StopwatchIncreaseChecker(Stopwatch stopwatch, ref int counter)
+    {
+        if (counter++ > RoutineCheckCount)
+        {
+            counter = 0;
+            if (stopwatch.ElapsedMilliseconds > 10)
+            {
+                return true;
+            }
+        }
+        return false;
+    }
+
+    public IEnumerator Generate(Stopwatch stopwatch, bool yieldOnSpendTimeTooLong)
+    {
+        // Profiler.BeginSample("Simulator.Generate => Test");
+        int counter = 0;
+
+        // init stopwatch checker
+        if (yieldOnSpendTimeTooLong)
+        {
+            stopwatch.Reset();
+            stopwatch.Start();
+        }
+
+        for (int i = int.MinValue; i < int.MaxValue; i++)
+        {
+            for (int j = int.MinValue; j < int.MaxValue; j++)
+            {
+                // if stopwatch spend too long, yield return
+                if (yieldOnSpendTimeTooLong && StopwatchIncreaseChecker(stopwatch, ref counter))
+                {
+                    yield return null;
+                    stopwatch.Reset();
+                    stopwatch.Start();
+                }
+            }
+        }
+
+        // Profiler.EndSample();
+    }
+}
+
---
+++ Assets/Scripts/SimulatorComponent.cs
@@ -0,0 +1,72 @@
+﻿using System.Diagnostics;
+using System.Collections;
+using System;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Serialization;
+
+
+public class SimulatorComponent : MonoBehaviour
+{
+    private Simulator simulator;
+    public Simulator GetSimulator()
+    {
+        if (simulator == null && Application.isPlaying)
+        {
+            simulator = new Simulator();
+        }
+        return simulator;
+    }
+
+    private Coroutine m_SimulatorCoroutine;
+    void OnEnable()
+    {
+        m_SimulatorCoroutine = StartCoroutine(RoutineRegenerateFlowField());
+    }
+
+    void OnDisable()
+    {
+        if (m_SimulatorCoroutine != null)
+        {
+            StopCoroutine(m_SimulatorCoroutine);
+            m_SimulatorCoroutine = null;
+        }
+    }
+
+    private bool m_IsCalculating = false;
+    private float m_LastRefreshTime = 0f;
+    private IEnumerator RoutineRegenerateFlowField()
+    {
+        Simulator flowField = GetSimulator();
+        Stopwatch stopwatch = new Stopwatch();
+
+        while (true)
+        {
+            if (Time.time - m_LastRefreshTime > 1)
+            {
+                m_IsCalculating = true;
+
+                yield return flowField.Generate(stopwatch, true);
+                // var gen = flowField.Generate(stopwatch, true);
+                // while (gen.MoveNext())
+                // {
+                //     yield return gen.Current;
+                // }
+
+                m_IsCalculating = false;
+
+                m_LastRefreshTime = Time.time;
+            }
+            yield return null;
+        }
+    }
+
+    private void OnDestroy()
+    {
+        if (simulator != null)
+        {
+            simulator.Dispose();
+            simulator = null;
+        }
+    }
+}
